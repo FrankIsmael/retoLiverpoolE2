710cc57dc247f82661d30e9304d415f48f34914e
Ismael <ismaelfcom93@gmail.com>
2019-07-08 06:31:57 -0500

    application working

--- liverpool-react/src/App.js
+++ liverpool-react/src/App.js
@@ -5,50 +5,84 @@ import Search from './components/Search';
 
 
 export default class App extends Component {
-  state = { products: [],
-    filter: ''
+  state = {
+    products: [],
+    history: [],
+    searchValue: "",
+    flag: false
   }
 
   getAllProducts = (filter) => {
     axios.get(`https://www.liverpool.com.mx/tienda/?s=${filter}&d3106047a194921c01969dfdec083925=json`)
       .then(responseFromApi => {
+        if(!responseFromApi.data.contents ) {this.setState({flag : true})}
+        else{this.setState({flag : false})}
         let array = responseFromApi.data.contents[0].mainContent
         var tam = array.length
         this.setState({
           products: responseFromApi.data.contents[0].mainContent[tam - 1].contents[0].records
         })
+        if(this.state.products.length === 0) this.setState({flag : true})
         console.log(this.state.products)
       })
       .catch(err => { return err })
   }
-  
+
   handleSearch = e => {
-    //this.setState({filter: e.target.value})
-    const searchTerm = e.target.value
-    this.getAllProducts(searchTerm);
+    this.setState({ searchValue: e.target.value });
+    this.getHistory()
   }
 
-  //componentDidMount() {
-    
-  //}
+  handleClick = () => {
+    this.getAllProducts(this.state.searchValue)
+    this.state.history.push(this.state.searchValue)
+    this.setHistory(this.state.history)
+  }
+
+  getHistory = () => {
+    let storedList = localStorage.getItem('history')
+    if(storedList == null){
+      this.setState({history: []})
+    } else{
+      this.setState({history: JSON.parse(storedList)})
+    }
+    return this.state.history
+  }
+
+  setHistory = (list) => {
+    localStorage.setItem('history', JSON.stringify(list))
+  }
+
+  componentDidMount(){
+    this.getHistory()
+  }
 
   render() {
     return (
       <div>
-        <Search handleSearch={this.handleSearch}/>
-        <div>{this.state.filter}</div>
-        
+        <Search handleSearch={this.handleSearch} handleClick={this.handleClick} getHistory={this.getHistory} />
+        <b>Busquedas:</b>
+        {this.state.history.map((search,i)=>{
+          return(
+            <span key={i}> {search} </span>
+          )
+        })}
+        {this.state.flag ?
+        <h1>No se obtuvieron resultados, Realize nueva busqueda</h1>
+         : 
+         <div>
+         {this.state.products.map((product, i) => {
+           return (
+             <div key={i}>
+               <img src={product.thumbnailImage[0]} alt='prod' width='100' />
+               <h1>{product.productDisplayName[0]}</h1>
+               <h1>{product.listPrice[0]}</h1>
+             </div>
+           )
+         })
+         }
+         </div>}
 
-        {this.state.products.map((product,i) => {
-                        return (
-                            <div key={i}>
-                                    <img src={product.thumbnailImage[0]} alt='prod' width='100' />
-                                    <h1>{product.productDisplayName[0]}</h1>
-                                    <h1>{product.listPrice[0]}</h1>
-                            </div>
-                        )
-                    })
-                    }
       </div>
     )
   }
--- liverpool-react/src/components/Search.js
+++ liverpool-react/src/components/Search.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-export default function Search({handleSearch}) {
+export default function Search({handleSearch, handleClick, getHistory}) {
     return (
         <div>
             <h1>Liverpool</h1>
             <label>Search</label><br/>
-            <input name='product' placeholder='search a product' onChange={(e)=> handleSearch(e)} /><br/>
+            <input name='searchs' placeholder='search a product' onChange={(e)=> handleSearch(e)} /><br/>
+            <button onClick={handleClick}>Buscar</button>
         </div>
     )
 }
